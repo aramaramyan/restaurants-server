a297dda0a1fb49de130f8075fe3549b5c77a8f64
aramaramyan <aramaramyan1997@mail.ru>
2022-07-27 11:03:58 +0400

    feat: create post request for restaurant reviews

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use('/restaurants', require('./routes/restaurants'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/restaurants', require('./routes/restaurants'));
+app.use('/reviews', require('./routes/reviews'));
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, (error) =>{
---
+++ src/routes/reviews.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const reviewService = require('../services/review')
+
+router.post(
+  '/',
+  (req, res) => {
+    const restaurants = reviewService.addReview(req.body.id, req.body.message);
+    res.status(200).send(restaurants);
+  }
+);
+
+module.exports = router;
