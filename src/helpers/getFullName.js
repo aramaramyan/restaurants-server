6d483c0f974bcaf71f5ef10251afdf9ea977f95a
aramaramyan <aramaramyan1997@mail.ru>
2022-07-28 10:13:32 +0400

    create getFullName function that generates random full name

---
+++ src/helpers/getFullName.js
@@ -0,0 +1,35 @@
+const names = [
+  "Aram", "Arman", "Albert", "Ani", "Amalia", "Armen", "Anahit",
+  "Bako", "Carine", "Chinar", "Gayane", "Gurgen", "Garnik", "Gaspar",
+  "Hakob", "Hasmik", "Hayk", "Hayarpi", "Knar", "Karen", "Lara", "Levon",
+  "Laura", "Meruj", "Mariam", "Meri", "Mayis", "Nver", "Narek", "Nane",
+  "Narine", "Paruyr", "Rafael", "Robert", "Roza", "Samvel", "Satenik", "Suren",
+  "Taron", "Tatev", "Tigran", "Vardan", "Vahe", "Vard",
+];
+const surnames = [
+  "Aramyan", "Abrahamyan", "Aleksanyan",
+  "Babayan", "Baghdasaryan", "Badalyan",
+  "Danielyan", "Darbinyan", "Davtyan",
+  "Gasparyan", "Gevorgyan", "Gabrielyan",
+  "Hovhannisyan", "Hakobyan", "Harutyunyan",
+  "Karapetyan", "Kirakosyan", "Kocharyan",
+  "Mirzoyan", "Mkhitaryan", "Mkrtchyan",
+  "Nazaryan", "Nersisyan", "Ohanyan",
+  "Petrosyan", "Poghosyan", "Safaryan",
+  "Sahakyan", "Samvelyan", "Sargsyan",
+  "Tonoyan", "Torosyan", "Tovmasyan",
+  "Vardanyan", "Voskanyan", "Yesayan",
+];
+
+function getRandomNumber(maxNum) {
+  return (Math.random() * maxNum) + 1;
+}
+
+function getFullName() {
+  const name = names[getRandomNumber(names.length)];
+  const surname = surnames[getRandomNumber(surnames.length)];
+
+  return name + surname;
+}
+
+module.exports = getFullName;
