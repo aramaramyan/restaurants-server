17f6507b75f5dd3b2c157307450578be70345dd5
aramaramyan <aramaramyan1997@mail.ru>
2022-07-28 14:19:19 +0400

    feat: add cors package

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/restaurants', require('./routes/restaurants'));
 app.use('/reviews', require('./routes/reviews'));
