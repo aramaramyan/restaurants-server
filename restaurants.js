4679c71681440d2d344dd76604ac8ab2f1823a0b
aramaramyan <aramaramyan1997@mail.ru>
2022-07-28 09:41:49 +0400

    modify: add some new properties to review object

--- restaurants.js
+++ restaurants.js
@@ -4,11 +4,15 @@ const restaurants = [
     "name": "Sherep",
     "reviews": [
       {
-        title: "One of the best places I visited. Service is top! Food so delicious, especially hinkaliDefinitely recommend to visit!",
+        name: "Name Surname",
+        rate: 5,
+        review: "One of the best places I visited. Service is top! Food so delicious, especially hinkaliDefinitely recommend to visit!",
         date: 1658897683122
       },
       {
-        title: "Everything was super delicious and the service is great. The fried khinkali is amazing. Our waiter Argijthi was very friendly and attentive.",
+        name: "Name Surname",
+        rate: 5,
+        review: "Everything was super delicious and the service is great. The fried khinkali is amazing. Our waiter Argijthi was very friendly and attentive.",
         date: 1658897704724
       }
     ],
@@ -31,11 +35,15 @@ const restaurants = [
     "name": "Yasaman",
     "reviews": [
       {
-        title: "I came here with my family and the food was amazing, reasonable price and excellent flavors, i would recommend you to come here.",
+        name: "Name Surname",
+        rate: 5,
+        review: "I came here with my family and the food was amazing, reasonable price and excellent flavors, i would recommend you to come here.",
         date: 1658897844839
       },
       {
-        title: "I've been in Yerevan so many times from 2019 this is my first that i eat here food is great and the environment is so lovely and cozy. I will come again and again",
+        name: "Name Surname",
+        rate: 4,
+        review: "I've been in Yerevan so many times from 2019 this is my first that i eat here food is great and the environment is so lovely and cozy. I will come again and again",
         date: 1658898674765
       }
     ],
@@ -58,17 +66,21 @@ const restaurants = [
     "name": "Seasons",
     "reviews": [
       {
-        title: "This was our 1st food experience in Yerevan. A place were authentic Armenian food meets with European cuisine. Excellent customer service as well as atmosphere. Price was fair.",
+        name: "Name Surname",
+        rate: 4,
+        review: "This was our 1st food experience in Yerevan. A place were authentic Armenian food meets with European cuisine. Excellent customer service as well as atmosphere. Price was fair.",
         date: 1658897961363
       },
       {
-        title: "Amazing service and delicious food. Good place for breakfast and brunch, dinner. They have also very tasty options of desserts.",
+        name: "Name Surname",
+        rate: 5,
+        review: "Amazing service and delicious food. Good place for breakfast and brunch, dinner. They have also very tasty options of desserts.",
         date: 1658898001994
       }
     ],
     "rates": [3, 4, 5],
     "phone": "+374 43 709070",
-    "location": "Mesrop Mashtots Ave., 15 At Seasons Park, Yerevan 0002 Armenia",
+    "location": "Mesrop Mashtots Ave. 15, Yerevan 0002 Armenia",
     "webPage": "https://www.yeremyanprojects.com/en/restaurants/seasons",
     "openTime": "12:00 PM - 12:00 AM",
     "kitchenType": ["European", "Armenian", "Contemporary"],
@@ -85,11 +97,15 @@ const restaurants = [
     "name": "el Garden",
     "reviews": [
       {
-        title: "This is a unique and beautiful place to chill, remember to do a reservation on weekends or else you won’t be able to find a spot!",
+        name: "Name Surname",
+        rate: 5,
+        review: "This is a unique and beautiful place to chill, remember to do a reservation on weekends or else you won’t be able to find a spot!",
         date: 1658898220646
       },
       {
-        title: "Beautiful place, nature, delicious food, Thank you for the garden. :\n" +
+        name: "Name Surname",
+        rate: 5,
+        review: "Beautiful place, nature, delicious food, Thank you for the garden. :\n" +
           "Aren was very hospitable. We also thank him.",
         date: 1658898246118
       },
@@ -117,17 +133,21 @@ const restaurants = [
     "name": "Mezzo",
     "reviews": [
       {
-        title: "Had a great time with friends. The live music was fantastics. Special thanks to Stepan for the warm welcome and fabulous service!!!",
+        name: "Name Surname",
+        rate: 5,
+        review: "Had a great time with friends. The live music was fantastics. Special thanks to Stepan for the warm welcome and fabulous service!!!",
         date: 1658898441357
       },
       {
-        title: "The music is good, the hall was cold and mainly why I leave 1 point , as we are here for about 2 hours and still waiting for our food!!! Don’t have words ...",
+        name: "Name Surname",
+        rate: 5,
+        review: "The music is good, the hall was cold and mainly why I leave 1 point , as we are here for about 2 hours and still waiting for our food!!! Don’t have words ...",
         date: 1658898475577
       }
     ],
     "rates": [3, 5, 5],
     "phone": "+374 10 524211",
-    "location": "st. Isahakyan, 28, 1/3 206-206, Yerevan 0009 Armenia",
+    "location": "st. Isahakyan 28, Yerevan 0009 Armenia",
     "webPage": "https://www.mezzo.am/",
     "openTime": "12:00 PM - 12:00 AM",
     "kitchenType": ["Sushi", "European", "Eastern European", "Central European", "Bar", "International"],
